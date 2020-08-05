ff55b91afcc10f17e231dce3f3193b3f633d540b
Miguel Brito <migueldiasbrito@gmail.com>
2020-08-06 00:17:23 +0100

    Has Cell Been Reached Condition

---
+++ Assets/Scripts/mdb/MyTactical/Controller/BasicAI/Conditions/HasCellBeenReached.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+using mdb.MyTactial.Model;
+
+namespace mdb.MyTactial.Controller.BasicAI.Conditions
+{
+    public class HasCellBeenReached : Condition
+    {
+        [SerializeField]
+        private int[] _cellIndex = new int[0];
+
+        private List<Cell> _cells;
+
+        private void Start()
+        {
+            _cells = new List<Cell>();
+
+            foreach (int cellIndex in _cellIndex)
+            {
+                _cells.Add(BattleController.instance.Battle.Cells[cellIndex]);
+            }
+
+            BattleStateMachine.instance.MoveUnit.OnExit += OnMoveUnitExit;
+        }
+
+        private void OnMoveUnitExit()
+        {
+            if (_cells.Contains(BattleController.instance.CurrentUnit.Cell))
+            {
+                TestCondition = true;
+                BattleStateMachine.instance.MoveUnit.OnExit -= OnMoveUnitExit;
+            }
+        }
+    }
+}
